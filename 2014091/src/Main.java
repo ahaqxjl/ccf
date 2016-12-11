c2fdbd9283fa4aa06d5e9dd8960cabd447782870
jordan <ahaqxjl@163.com>
2016-12-11 12:34:53 +0800

    init 20140901

---
+++ 2014091/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = Integer.parseInt(scanner.nextLine());
+		int[] numbers = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			numbers[i] = scanner.nextInt();
+		}
+		
+		int result = 0;
+		
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (numbers[i] - numbers[j] == 1 || numbers[j] - numbers[i] == 1) {
+					result ++;
+				}
+			}
+		}
+		
+		System.out.println(result);
+		
+		scanner.close();
+	}
+}
